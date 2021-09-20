a8c627b5651f60b4ac2e5535ccc63a74bc34dcc7
jpoulin587 <jpoulin587a@gmail.com>
2021-09-20 12:43:55 -0500

    get routes working

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,26 @@
 const router = require("express").Router();
-const Workout = require("../models");
+const Workout = require("../models/workout");
+//const path = require('path');
+
+router.get("/api/workouts", (req, res)=> {
+    Workout.find()
+        .then (workouts => {
+            res.json(workouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts/range", (req, res)=> {
+    Workout.find({})
+        .then (workouts => {
+            res.json(workouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 
 
